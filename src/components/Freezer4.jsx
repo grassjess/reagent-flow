d981473f9350a558585d2ac1583de6e76c809ad4
grassjess <gsk.souza@gmail.com>
2025-05-16 10:47:54 -0400

    Update Freezer4.jsx

--- src/components/Freezer4.jsx
+++ src/components/Freezer4.jsx
@@ -1,1 +1,31 @@
+import React from 'react';
+import GoBackButton from './GoBackButton';
 
+export default function Freezer4({ onGoBack }) {
+  const names = ['Headwig', 'Hermione', 'Mavalo'];
+
+  return (
+    <div style={{ backgroundColor: '#E6F0F4', height: '100vh', paddingTop: 100 }}>
+      {names.map(name => (
+        <div
+          key={name}
+          style={{
+            width: 684,
+            height: 150,
+            margin: '20px auto',
+            background: '#F5F5F5',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            fontSize: 48,
+            fontFamily: 'Inter',
+            color: '#4A4A4A',
+          }}
+        >
+          {name}
+        </div>
+      ))}
+      <GoBackButton onClick={onGoBack} />
+    </div>
+  );
+}
