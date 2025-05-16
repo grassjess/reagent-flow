fc9a12a47e0ee28763c1f0b0eda9c56a3aec54a6
grassjess <gsk.souza@gmail.com>
2025-05-16 10:48:13 -0400

    Update GoBackButton.jsx

--- src/components/GoBackButton.jsx
+++ src/components/GoBackButton.jsx
@@ -1,1 +1,24 @@
+import React from 'react';
 
+export default function GoBackButton({ onClick }) {
+  return (
+    <div
+      onClick={onClick}
+      style={{
+        width: 348,
+        height: 81,
+        margin: '40px auto',
+        background: '#FFB5A7',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 32,
+        fontFamily: 'Inter',
+        color: '#4A4A4A',
+        cursor: 'pointer',
+      }}
+    >
+      Go Back
+    </div>
+  );
+}
